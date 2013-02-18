fce015934f0d4165256a11c05574de285b915afa
zhaojie <yiwenshengmei@163.com>
2013-02-18 15:17:40 +0800

    * 在自定义拦截器中实现登录验证  * 加入json类型的result视图

---
+++ src/com/zj/retrieval/master/actions/NodeAction.java
@@ -0,0 +1,10 @@
+package com.zj.retrieval.master.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class NodeAction {
+	
+	public String execute() {
+		return ActionSupport.SUCCESS;
+	}
+}
